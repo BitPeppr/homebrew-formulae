207a0357b9d5a23f5cf5be196bda4689a17fc63d
Peppr <martin.chen.au@gmail.com>
2026-03-09 20:54:36 +1100

    Update URL for cautom package source

--- Formula/cautom.rb
+++ Formula/cautom.rb
@@ -1,7 +1,7 @@
 class Cautom < Formula
   desc "Terminal Game of Life simulator"
   homepage "https://github.com/Shiyuan-Chen-17/CAutom"
-  url "https://github.com/Shiyuan-Chen-17/Cellular-automaton/releases/download/v1.1.0/cautom-1.1.0-macos-arm64.tar.gz"
+  url "https://github.com/BitPeppr/Cellular-automaton/releases/download/v1.1.0/cautom-1.1.0-macos-arm64.tar.gz"
   sha256 "b6f05b5a3590c5ea3684a478662414f968bd4da7f00539c36ced5e13c2490cc9"
   version "1.0.0"
 
