22ad6ef6639413cc3016aa64b937f40a0565878b
Peppr <martin.chen.au@gmail.com>
2026-04-10 17:45:42 +1000

    Update rosella.rb

--- Formula/rosella.rb
+++ Formula/rosella.rb
@@ -2,7 +2,7 @@ class Rosella < Formula
   include Language::Python::Virtualenv
 
   desc "Terminal boids simulator"
-  homepage "https://github.com/martinchen/rosella"
+  homepage "https://github.com/bitpeppr/rosella"
   url "https://files.pythonhosted.org/packages/81/8a/ab509535de9ef1e4a7c159c9bf3e0ca59e142abd307a9e5055733c48656e/rosella-0.1.1.tar.gz"
   sha256 "0961412077fcd733945c481df8f35f7a5803d3c134f840fdb3b34114b5f28476"
   license "MIT"
@@ -10,7 +10,7 @@ class Rosella < Formula
   depends_on "python@3.12"
 
   resource "drawille" do
-    url "https://files.pythonhosted.org/packages/d3/2f/46b807441ea28af8c890e88d430e65b79b5714d226aacd032823cd4c6b7e/drawille-0.2.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/08/57/1cb2c11bab5bd5b9de979f8a2205216c372f4f531e90cddb2e721871f9b2/drawille-0.2.0.tar.gz"
     sha256 "815343e1162c58052d4cb652ef970f6771edd90042d549ecc6e453c9736aa1ad"
   end
 
